0adbb665402cad04337c1a3919319ba5952111e6
Rajat <rpandey@mdsol.com>
2020-01-11 13:15:44 +0530

    Initial Setup for Test

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+//entry --> output
+const path  = require('path')
+
+module.exports= {
+ entry: './src/app.js',
+ output: {
+   path: path.join(__dirname, 'public'),  //path to the bundle of the output file
+   filename: 'bundle.js' //this is the filename of the bundle file
+  },
+  module: {
+    rules: [{
+      loader: 'babel-loader',
+      test: /\.js$/,
+      exclude: /node_modules/
+    }]
+  }
+}
